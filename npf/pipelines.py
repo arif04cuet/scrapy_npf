a7a6da8003246efbc094ce7a1fea65dafdf3246d
Arif Hossain <arif04cuet@gmail.com>
2017-10-20 14:42:01 +0600

    update

--- npf/pipelines.py
+++ npf/pipelines.py
@@ -6,31 +6,85 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+# import sys
+# import MySQLdb
+# import hashlib
+# from scrapy.exceptions import DropItem
+# from scrapy.http import Request
 
 
-import sys
-import MySQLdb
-import hashlib
-from scrapy.exceptions import DropItem
-from scrapy.http import Request
-
-class NpfPipeline(object):
-  def __init__(self):
-    self.conn = MySQLdb.connect('localhost', 'root', 'root', 'scrapy', charset="utf8", use_unicode=True)
-    self.cursor = self.conn.cursor()
-
-def process_item(self, item, spider):    
-    try:
-        self.cursor.execute("""INSERT INTO example_book_store (book_name, price)  
-                        VALUES (%s, %s)""", 
-                       (item['book_name'].encode('utf-8'), 
-                        item['price'].encode('utf-8')))
-
-        self.conn.commit()
+# class NpfPipeline(object):
+#     def __init__(self):
+#         self.conn = MySQLdb.connect(
+#             'localhost', 'root', 'root', 'scrapy', charset="utf8", use_unicode=True)
+#         self.cursor = self.conn.cursor()
 
 
-    except MySQLdb.Error, e:
-        print "Error %d: %s" % (e.args[0], e.args[1])
+# def process_item(self, item, spider):
+#     try:
+#         self.cursor.execute("""INSERT INTO example_book_store (book_name, price)
+#                         VALUES (%s, %s)""",
+#                             (item['book_name'].encode('utf-8'),
+#                              item['price'].encode('utf-8')))
+
+#         self.conn.commit()
+
+#     except MySQLdb.Error, e:
+#         print "Error %d: %s" % (e.args[0], e.args[1])
+
+#     return item
 
 
-    return item
+from twisted.enterprise import adbapi
+import datetime
+import pymysql.cursors
+import logging
+
+
+class SQLStorePipeline(object):
+
+    def __init__(self):
+        self.connection = pymysql.connect(
+            "localhost", "root", "root", "scrapy", charset='utf8')
+        self.cursor = self.connection.cursor()
+
+    def process_item(self, item, spider):
+
+        data = []
+        for row in item['links']:
+
+            link = {}
+            link['status'] = 200
+            link['isExternal'] = 0
+            link['hasData'] = 1
+
+            link['link'] = row['link']
+
+            if link['link'].startswith('http'):
+                link['isExternal'] = 1
+                link['status'] = 200
+            elif not link['link'].startswith('/site/'):
+                link['status'] = 404
+            else:
+                link['status'] = 200
+
+            data.append((
+                row['domain'],
+                row['firstLabel'],
+                row['secondLabel'],
+                row['title'],
+                row['link'],
+                link['status'],
+                link['hasData'],
+                link['isExternal']
+            ))
+
+        stmt = "INSERT INTO links (domain, firstLabel,secondLabel,title,link,status,hasData,isExternal) VALUES (%s, %s,%s, %s,%s,%s,%s,%s)"
+        self.cursor.executemany(stmt, data)
+        self.connection.commit()
+        # logging.info(data)
+        return item
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.connection.close()
--- npf/spiders/npf_spider.py
+++ npf/spiders/npf_spider.py
@@ -4,11 +4,14 @@ from npf.items import NpfItem, DomainItem
 
 class NpfSpider(scrapy.Spider):
     name = "npf"
+    #start_urls = ['http://www.comilla.gov.bd']
+
     def start_requests(self):
         for url in open('urls.txt'):
-                yield scrapy.Request(url, self.parse)
+            yield scrapy.Request(url.strip(), self.parse)
 
     def parse(self, response):
+        items = []
         self.logger.info('Parse function called on %s', response.url)
         # Main Manu
         for firstLabel in response.css('#dawgdrops>ul>li'):
@@ -16,14 +19,18 @@ class NpfSpider(scrapy.Spider):
                 for thirdLabel in secondLabel.css('ul>li'):
                     item = NpfItem()
                     item['domain'] = response.url
-                    item['firstLabel'] = firstLabel.css('a::attr(title)').extract_first()
-                    item['secondLabel'] = secondLabel.css("h6::text").extract_first()
-                    item['title'] = thirdLabel.css("a::attr(title)").extract_first()
-                    item['link'] = thirdLabel.css("a::attr(href)").extract_first()
-                    yield item
-                    #items.append(item)
+                    item['firstLabel'] = firstLabel.css(
+                        'a::attr(title)').extract_first()
+                    item['secondLabel'] = secondLabel.css(
+                        "h6::text").extract_first()
+                    item['title'] = thirdLabel.css(
+                        "a::attr(title)").extract_first()
+                    item['link'] = thirdLabel.css(
+                        "a::attr(href)").extract_first()
+                    # yield item
+                    items.append(item)
 
-        #Service Box
+        # Service Box
         for box in response.css('div.box'):
             for link in box.css('ul>li'):
                 item = NpfItem()
@@ -32,7 +39,8 @@ class NpfSpider(scrapy.Spider):
                 item['secondLabel'] = box.css("h4::text").extract_first()
                 item['title'] = link.css("a::text").extract_first()
                 item['link'] = link.css("a::attr(href)").extract_first()
-                yield item
+                # yield item
+                items.append(item)
 
         # Right Bar
         for box in response.css('#right-content>div.right-block'):
@@ -45,7 +53,8 @@ class NpfSpider(scrapy.Spider):
                     item['secondLabel'] = box.css("h5::text").extract_first()
                     item['title'] = link.css("a::text").extract_first()
                     item['link'] = link.css("a::attr(href)").extract_first()
-                    yield item
+                    # yield item
+                    items.append(item)
 
             else:
                 for link in box.css('a.share-buttons'):
@@ -55,4 +64,10 @@ class NpfSpider(scrapy.Spider):
                     item['secondLabel'] = 'Social Media'
                     item['title'] = link.css("img::attr(alt)").extract_first()
                     item['link'] = link.css("::attr(href)").extract_first()
-                    yield item
+                    # yield item
+                    items.append(item)
+
+        domain = DomainItem()
+        domain['name'] = items[0]['domain']
+        domain['links'] = items
+        yield domain
