58bec558f988bfc1f7964c282036b6dcccff3946
Arif Hossain <arif04cuet@gmail.com>
2017-10-30 15:34:22 +0600

    updated

--- npf/cr.py
+++ npf/cr.py
@@ -8,76 +8,118 @@ import requests
 import pymysql
 import logging
 import csv
-import os;
+import os
+import grequests
+import time
 
 concurrent = 500
+q = queue.Queue(concurrent * 2)
 data = []
+connection = pymysql.connect(
+    "localhost", "root", "root", "scrapy", charset='utf8')
+cursor = connection.cursor()
+
+fetchLimit = 100000
+
 
 def doWork():
     while True:
         row = q.get()
         if len(row) == 6:
-            d,firstLabel,secondLabel,title,url,isExternal = row
+            d, firstLabel, secondLabel, title, url, isExternal = row
             isExternal = int(isExternal)
-            status,hd,url = getStatus(d,url,isExternal)
-            doSomethingWithResult(d,firstLabel,secondLabel,title,url,status,hd,isExternal)
+            status, hd, url = getStatus(d, url, isExternal)
+            doSomethingWithResult(d, firstLabel, secondLabel,
+                                  title, url, status, hd, isExternal)
         else:
             print(row)
 
         q.task_done()
 
 
-def getStatus(d,ourl,isExternal):
+def getStatus(d, ourl, isExternal):
     try:
 
         url = ourl
         if not isExternal:
-           url = d+ourl
-            
+            url = d + ourl
+
         if isExternal:
             res = requests.head(url)
         else:
-            res = requests.get(url)    
+            res = requests.get(url)
         print (url)
         return res.status_code, len(res.content), ourl
 
     except requests.exceptions.RequestException as e:
         return 0, 0, ourl
 
-def doSomethingWithResult(d,firstLabel,secondLabel,title,url,status,hd,isExternal):
-    
-     data.append((d,firstLabel,secondLabel,title,url,status,hd,isExternal,1))
-   
+
+def doSomethingWithResult(d, firstLabel, secondLabel, title, url, status, hd, isExternal):
+
+    data.append((d, firstLabel, secondLabel, title,
+                 url, status, hd, isExternal, 1))
 
 
-q = queue.Queue(concurrent * 2)
-for i in range(concurrent):
-    t = Thread(target=doWork)
-    t.daemon = True
-    t.start()
-try:
-    connection = pymysql.connect("localhost","root","root","scrapy",charset='utf8')
-    cursor = connection.cursor()
+def startTaskParallal(dataLIst):
+    urls = []
+    for row in dataLIst:
+        d, firstLabel, secondLabel, title, url, isExternal = row
+        isExternal = int(isExternal)
+        if not isExternal:
+            url = d + url
+        urls.append(url)
 
-    
+    rs = (grequests.get(u) for u in urls)
+    #res = grequests.map(rs)
+    for res in grequests.imap(rs):
+
+        if res is not None:
+            url = res.url
+            print (url)
+            data.append((d, firstLabel, secondLabel, title,
+                         url, res.status_code, len(res.content), isExternal, 1))
+
+    # insertUpdate(data)
+    # data.clear()
 
 
-    sql = "select domain,firstLabel,secondLabel,title,link,isExternal from links where status !=404 and crawled=0 order by domain,isExternal limit 5000";
-    cursor.execute(sql)
-    result=cursor.fetchall()
-    
-    for row in result:
-             q.put(row)
-    q.join()
+def startTask(dataLIst):
 
-    #print(data)
+    for i in range(concurrent):
+        t = Thread(target=doWork)
+        t.daemon = True
+        t.start()
+    try:
 
+        for row in dataLIst:
+            q.put(row)
+        q.join()
+
+        insertUpdate(data)
+        data.clear()
+
+    except KeyboardInterrupt:
+        sys.exit(1)
+
+
+def insertUpdate(dataSet):
     stmt = "INSERT INTO tmp_links (domain,firstLabel,secondLabel,title,link,status,hasData,isExternal,crawled) VALUES (%s, %s,%s, %s,%s,%s, %s,%s, %s)"
-    cursor.executemany(stmt, data)
-
-    sql = "update links set crawled=1 where crawled=0 limit 5000";
+    cursor.executemany(stmt, dataSet)
+    sql = "update links set crawled=1 where status !=404 and isExternal=0 and crawled=0 limit %s" % fetchLimit
     cursor.execute(sql)
     connection.commit()
-    
-except KeyboardInterrupt:
-    sys.exit(1)
+
+
+def getLinks():
+    start = time.time()
+    sql = "select domain,firstLabel,secondLabel,title,link,isExternal from links where status !=404 and isExternal=0 and crawled=0 order by id limit %s" % fetchLimit
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    startTaskParallal(result)
+    end = time.time()
+    print(end - start)
+
+
+for i in range(0, 1):
+    getLinks()
--- npf/spiders/npf_spider.py
+++ npf/spiders/npf_spider.py
@@ -3,29 +3,30 @@ from npf.items import NpfItem, DomainItem
 import pymysql
 import logging
 
+
 class NpfSpider(scrapy.Spider):
     name = "npf"
     #start_urls = ['http://www.comilla.gov.bd']
 
-    def removeWhiteSpace(self,word):
+    def removeWhiteSpace(self, word):
         if word is None:
-            return '';
-        return word.strip().strip("\'").strip('\"').replace("'",'').replace('"','').replace(',','')
+            return ''
+        return word.strip().strip("\'").strip('\"').replace("'", '').replace('"', '').replace(',', '')
 
     def start_requests(self):
-        db = pymysql.connect("localhost","root","root","scrapy" )
+        db = pymysql.connect("localhost", "root", "root", "scrapy")
         cursor = db.cursor()
-        sql = "select domain from scraped_domains where crawled=0 order by field(domain_type,'Upazilla','District','Division') desc";
+        sql = "select domain from scraped_domains where crawled=0 and domain_type in ('Division','District','Upazilla') order by FIND_IN_SET(domain_type,'Division,District,Upazilla,Union')"
         cursor.execute(sql)
-        result=cursor.fetchall()
+        result = cursor.fetchall()
         for row in result:
-            url = 'http://'+row[0];
-            
+            url = 'http://' + self.removeWhiteSpace(row[0])
+
             yield scrapy.Request(url.strip(), self.parse)
 
     def parse(self, response):
 
-        if response.css('#dawgdrops>ul>li'):
+        if response.css('#dawgdrops>ul>li') or response.css('div.box') or response.css('#right-content>div.right-block'):
             items = []
             self.logger.info('Parse function called on %s', response.url)
             # Main Manu
@@ -51,14 +52,17 @@ class NpfSpider(scrapy.Spider):
                     item = NpfItem()
                     item['domain'] = response.url
                     item['firstLabel'] = 'Service Box'
-                    item['secondLabel'] = self.removeWhiteSpace(box.css("h4::text").extract_first())
-                    item['title'] = self.removeWhiteSpace(link.css("a::text").extract_first())
-                    item['link'] = self.removeWhiteSpace(link.css("a::attr(href)").extract_first())
+                    item['secondLabel'] = self.removeWhiteSpace(
+                        box.css("h4::text").extract_first())
+                    item['title'] = self.removeWhiteSpace(
+                        link.css("a::text").extract_first())
+                    item['link'] = self.removeWhiteSpace(
+                        link.css("a::attr(href)").extract_first())
                     items.append(item)
 
             # Right Bar
             for box in response.css('#right-content>div.right-block'):
-               
+
                 ulli = box.css('ul>li')
                 if ulli:
                     for link in box.css('ul>li'):
@@ -67,10 +71,11 @@ class NpfSpider(scrapy.Spider):
                         item['firstLabel'] = 'Right Bar'
                         item['secondLabel'] = self.removeWhiteSpace(box.css(
                             "h5::text").extract_first())
-                        item['title'] = self.removeWhiteSpace(link.css("a::text").extract_first())
+                        item['title'] = self.removeWhiteSpace(
+                            link.css("a::text").extract_first())
                         item['link'] = self.removeWhiteSpace(link.css(
                             "a::attr(href)").extract_first())
-    
+
                         if item['secondLabel'] != 'কেন্দ্রীয় ই-সেবা':
                             items.append(item)
 
@@ -82,7 +87,8 @@ class NpfSpider(scrapy.Spider):
                         item['secondLabel'] = 'Social Media'
                         item['title'] = self.removeWhiteSpace(link.css(
                             "img::attr(alt)").extract_first())
-                        item['link'] = self.removeWhiteSpace(link.css("::attr(href)").extract_first())
+                        item['link'] = self.removeWhiteSpace(
+                            link.css("::attr(href)").extract_first())
                         items.append(item)
 
             domain = DomainItem()
