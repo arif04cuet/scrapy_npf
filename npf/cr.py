b5e8d86a2e746a1ea0352c3fb923560ba791df04
Arif Hossain <arif04cuet@gmail.com>
2017-11-07 17:03:01 +0600

    updated

--- npf/cr.py
+++ npf/cr.py
@@ -10,11 +10,13 @@ import os
 import grequests
 import time
 from urllib.parse import urlparse, parse_qs
+from bs4 import BeautifulSoup
 
 concurrent = 100
 q = queue.Queue(concurrent * 2)
 
-fetchLimit = 100000
+fetchLimit = 30000
+insert_chunk_size = 1000
 data = []
 ids = []
 
@@ -22,31 +24,40 @@ connection = pymysql.connect(
     "localhost", "root", "root", "scrapy", charset='utf8')
 cursor = connection.cursor()
 
+def getContentLength(html):
+    dom = BeautifulSoup(html, 'html.parser')
+    content_area = dom.find(None, {"id":"printable_area"})
+    if content_area is not None:
+        return len(content_area.renderContents())
+    else:
+        return 0
 
 def getLinks():
-    sql = "select id,domain,firstLabel,secondLabel,title,link,isExternal from tmp_links where status !=404 and isExternal=0 order by domain limit %s" % fetchLimit
+    sql = "select id,domain,firstLabel,secondLabel,title,link,isExternal from tmp_links order by domain limit %s" % fetchLimit
     cursor.execute(sql)
     result = cursor.fetchall()
     return result
 
 def copy404rows():
-    sql = 'insert into links (domain,firstLabel,secondLabel,title,link,status,hasData,isExternal) select domain,firstLabel,secondLabel,title,link,status,hasData,isExternal from links where status=404'
+    sql = 'insert into links (domain,firstLabel,secondLabel,title,link,status,hasData,isExternal) select domain,firstLabel,secondLabel,title,link,status,hasData,isExternal from tmp_links where status=404'
     cursor.execute(sql)
     sql = 'delete from tmp_links where status=404'
     cursor.execute(sql)
-
+    connection.commit()
 
 def storeData():
     
     if data:
         stmt = "INSERT INTO links (domain,firstLabel,secondLabel,title,link,status,hasData,isExternal) VALUES (%s, %s,%s, %s,%s,%s, %s,%s)"
         cursor.executemany(stmt, data)
+      
 
     if ids:
         sql = 'delete from tmp_links where id in (' + ','.join(
             map(str, ids)) + ')'
         cursor.execute(sql)
         connection.commit()
+       
 
 
 
@@ -60,18 +71,16 @@ def doWork():
             url = link
             if not isExternal:
                 url = d + link
-
+            #print(url)
             status, hd = getStatus(d, url, isExternal)
             item = (d, firstLabel, secondLabel, title,
-                        link, status, hd, isExternal, 1)
+                        link, status, hd, isExternal)
 
             data.append(item)
             ids.append(id)
-            print(len(ids))
-            if(len(ids) == fetchLimit-1):
-                storeData()
-                data.clear()
-                ids.clear()
+            print(len(data))
+          
+               
         else:
             print(row)
 
@@ -81,15 +90,16 @@ def doWork():
 def getStatus(d, url, isExternal):
     
     try:
-        time.sleep(0.05)
+        #time.sleep(0.05)
         if isExternal:
-            res = requests.head(url)
+            res = requests.head(url,allow_redirects=False,timeout=15)
+            return res.status_code, 0
         else:
-            res = requests.get(url)
-
-        return res.status_code, len(res.content)
+            res = requests.get(url,allow_redirects=False,timeout=15)
+            return res.status_code, getContentLength(res.content)
 
     except requests.exceptions.RequestException as e:
+        print(e)
         return 0, 0
 
 
@@ -127,7 +137,10 @@ def startTaskParallal(dataLIst):
                 data.append((d, firstLabel, secondLabel, title,
                             link, res.status_code, len(res.content), isExternal))
                 ids.append(id)
-                #print(len(ids))  
+                if(len(ids) == insert_chunk_size):
+                    storeData()
+                    data.clear()
+                    ids.clear()
 
 
 def startTask(dataLIst):
@@ -142,9 +155,6 @@ def startTask(dataLIst):
             q.put(row)
         q.join()
 
-        #insertUpdate(data)
-        #data.clear()
-
     except KeyboardInterrupt:
         sys.exit(1)
 
@@ -154,7 +164,7 @@ copy404rows()
 
 for i in range(0, 1):
     rows = getLinks()
-    startTaskParallal(rows)
+    startTask(rows)
     storeData()
 
 cursor.close()
